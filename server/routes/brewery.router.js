b32aa894eaf410dc218ca029fae2715df58740ee
Brandon Wharton <whartonbm@gmail.com>
2021-06-28 12:02:06 -0500

    Brewery saga is receiving the correct data from DB

--- server/routes/brewery.router.js
+++ server/routes/brewery.router.js
@@ -13,8 +13,15 @@ router.get('/:id', rejectUnauthenticated, (req, res) => {
     // sanitized SQL string to get a single brewery
     const queryText = `SELECT * FROM "brewery" WHERE "id" = $1;`;
     // GET request to DB using provided id
-    console.log(queryText, [req.params.id]);
-    res.sendStatus(200);
-})
+    pool.query(queryText, [req.params.id])
+        .then(result => {
+            // send back the result
+            res.send(result.rows);
+        })
+        .catch(error => {
+            console.log('ERROR: GET single brewery', error);
+            res.sendStatus(500);
+        })
+});
 
 module.exports = router;
