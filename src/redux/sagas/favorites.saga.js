68967abca2ad09e1d3c3db19dc12b102cf7b9d74
Brandon Wharton <whartonbm@gmail.com>
2021-06-28 17:06:42 -0500

    Changed a comment

--- src/redux/sagas/favorites.saga.js
+++ src/redux/sagas/favorites.saga.js
@@ -7,8 +7,7 @@ function* addToFavorites (action) {
     try {
         // axios request to add a brewery to the user's favorites
         yield axios.post(`/api/favorites`, {breweryId: action.payload});
-        // update comments reducer with new data for the brewery that was just added to
-        // yield put( {type: 'FETCH_BREWERY_COMMENTS', payload: action.payload.breweryId })
+        // TO DO: Update favorites on DOM
     } catch (error) {
         console.error('Error with addToFavorites in favoritesSaga', error);
     }
