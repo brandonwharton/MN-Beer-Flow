76ed0c4cccd235f4a5bf8ac55ebe1efc91dfad49
Brandon Wharton <whartonbm@gmail.com>
2021-06-28 16:08:41 -0500

    Made a new Button to dispatch the breweryID and the action to make a new favorite, figured out eh SQL syntax for an UPSERT

--- src/components/AddToFavorites/AddToFavorites.jsx
+++ src/components/AddToFavorites/AddToFavorites.jsx
@@ -1,10 +1,31 @@
+// hooks
+import { useDispatch } from 'react-redux';
+// Material-UI components
+import Button from '@material-ui/core/Button';
+
 
 // component for an AddToFavorites button on the BreweryDetails view that adds the current brewery to user's 
 // favorites list
 function AddToFavorites({breweryId}) {
+    const dispatch = useDispatch();
+
+    // click handler for Add to Favorites button
+    const handleClick = () => {
+        // send a dispatch to add brewery to the user's favorites
+        dispatch({ type: 'CREATE_NEW_FAVORITE', payload: breweryId });
+    }
 
     return (
-        <h3>In AddToFavorites: {breweryId}</h3>
+        <>
+            <Button
+                variant="contained"
+                color="primary"
+                onClick={handleClick}
+            >
+                Add to Favorites
+            </Button>
+        </>
+
     )
 } 
 
