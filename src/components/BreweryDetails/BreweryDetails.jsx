fbf970e66188fc38d8c177e2780f1d7468928159
Brandon Wharton <whartonbm@gmail.com>
2021-06-28 11:34:50 -0500

    Set up page to dispatch a GET request, brought in id with useParams

--- src/components/BreweryDetails/BreweryDetails.jsx
+++ src/components/BreweryDetails/BreweryDetails.jsx
@@ -1,4 +1,20 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useParams } from "react-router";
+
 function BreweryDetails() {
+    // need to : useDispatch to GET details
+    // useSelector to use reducer
+    // make reducer/saga 
+    // make GET routes and router
+    const dispatch = useDispatch();
+    // hold database ID for page that was navigated to
+    const { id } = useParams();
+
+    // on navigation to specific details page, fetch details for specified brewery
+    useEffect(()=> {
+        dispatch({type: 'FETCH_SINGLE_BREWERY', payload: id});
+    })
 
     return (
         <h2>In BreweryDetails</h2>
