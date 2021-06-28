a373b691191564a2afec40c3415fa6c4618c2aea
Brandon Wharton <whartonbm@gmail.com>
2021-06-28 14:45:16 -0500

    Made a TextField and a the change handler/state needed to track it, made a Button that dispatches the new comment to comments reducer

--- src/components/AddComment/AddComment.jsx
+++ src/components/AddComment/AddComment.jsx
@@ -1,12 +1,50 @@
+// hooks
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+// Material-UI components
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+
+
+
+
+
 // component for a form to add a comment to a brewery
-
 function AddComment({breweryId}) {
+    const dispatch = useDispatch();
+    // state to track changes to TextField
+    const [newComment, setNewComment] = useState('');
 
-    console.log('in addComment for:', breweryId);
+    // change handler for comment TextField
+    const handleChange = (event) => {
+        // set state to whatever is being typed into TextField by user
+        setNewComment(event.target.value);
+    }
+
+    // click handler to dispatch newComment to comments saga
+    const handleClick = () => {
+        dispatch({ type: 'CREATE_NEW_COMMENT', payload: newComment });
+        // clear input
+        setNewComment('');
+    }
+
+
     return (
         <div>
-        
-            <h3>In AddComment</h3>
+            <TextField
+                label="leave a comment"
+                onChange={handleChange}
+                value={newComment}
+            >
+            </TextField>
+            <Button
+                variant="contained"
+                color="primary"
+                onClick={handleClick}
+            >
+                Add Comment
+            </Button>
         </div>
 
     )
