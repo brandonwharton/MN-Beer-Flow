0df3be43b733be364fd00c3a927b5ee203a65412
Brandon Wharton <whartonbm@gmail.com>
2021-06-29 13:33:53 -0500

    made cards to display user's favorite breweries

--- src/components/MyFavoritesItem/MyFavoritesItem.jsx
+++ src/components/MyFavoritesItem/MyFavoritesItem.jsx
@@ -1,9 +1,52 @@
+// Material-UI components
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent'
+// will likely need card actions for ratings
+
+
+// Material-UI styles
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        alignItems: 'center',
+        alignContent: 'center',
+    },
+    control: {
+        padding: theme.spacing(2),
+    },
+    card: {
+        backgroundColor: '#aaa',
+    },
+    media: {
+        height: 0,
+        paddingTop: '56.25%',
+    },
+}));
 
 // component that displays the clickable cards on MyFavoritesList
 function MyFavoritesItem({brewery}) {
+    // use the correct Material-UI styles
+    const classes = useStyles();
 
+    console.log(brewery);
     return (
-        <h3>My Favorite: {brewery.name}</h3>
+        <Card className={classes.card}>
+            <CardHeader
+                title={brewery.name}
+            >
+            </CardHeader>
+            <CardMedia
+                className={classes.media}
+                image={brewery.image_url}
+            />
+            <CardContent>
+                {/* Ratings details will go here */}
+            </CardContent>
+        </Card>
     )
 }
 
