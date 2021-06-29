7ca05868534d91f9d8b3d571326fce42effb9683
Brandon Wharton <whartonbm@gmail.com>
2021-06-29 16:01:36 -0500

    Made a form for handling search input and a button, made handlers for both, created local state for holding input changes and search results

--- src/components/MyFavoritesList/MyFavoritesList.jsx
+++ src/components/MyFavoritesList/MyFavoritesList.jsx
@@ -1,9 +1,12 @@
 // hooks
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 // Material-UI components
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 // components
 import MyFavoritesItem from '../MyFavoritesItem/MyFavoritesItem';
@@ -26,6 +29,9 @@ function MyFavoritesList() {
     const dispatch = useDispatch();
     // access data from brewery reducer
     const favoriteBreweryList = useSelector(store => store.breweryList)
+    // states for handling the search input and the filtered search results array
+    const [searchInput, setSearchInput] = useState('');
+    const [searchedArray, setSearchedArray] = useState([]);
 
     // on page load, get user's favorites
     useEffect(() => {
@@ -33,12 +39,38 @@ function MyFavoritesList() {
         dispatch({ type: 'FETCH_USER_FAVORITES' });
     }, [])
 
-    console.log(favoriteBreweryList);
+    // change handler for search input
+    const handleChange = (event) => {
+        // adjust search input state on input change
+        setSearchInput(event.target.value);
+    }
+
+    // submit handler for the search favorites feature
+    const handleSearch = () => {
+        console.log('clicked');
+    }
+
+    
     return (
         <div>
             <Typography variant="h3" component="h3">
                 My Favorites
             </Typography>
+            <FormControl onSubmit={handleSearch}>
+                <TextField 
+                    label="search favorites"
+                    helperText="by name only"
+                    value={searchInput}
+                    onChange={handleChange}
+                />
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={handleSearch}
+                >
+                    Search
+                </Button>
+            </FormControl>
             <Grid container className={classes.root} spacing={2} justify={'center'}>
                 <Grid item xs={10}>
                     {favoriteBreweryList?.map(brewery => (
