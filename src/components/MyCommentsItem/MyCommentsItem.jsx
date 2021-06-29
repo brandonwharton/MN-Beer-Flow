0e03312827ebaf322d21338434f75052159d8ced
Brandon Wharton <whartonbm@gmail.com>
2021-06-29 14:26:46 -0500

    Made a MyCommentsItem component to create comments for MyCommentsList, mapped into the new component

---
+++ src/components/MyCommentsItem/MyCommentsItem.jsx
@@ -0,0 +1,10 @@
+
+// component for the individual cards that are shown on the MyCommentsList view
+function MyCommentsItem({comment}) {
+
+    return (
+        <h3>{comment.comment}</h3>
+    )
+}
+
+export default MyCommentsItem;
--- src/components/MyCommentsList/MyCommentsList.jsx
+++ src/components/MyCommentsList/MyCommentsList.jsx
@@ -1,6 +1,8 @@
 // hooks
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+// components
+import MyCommentsItem from '../MyCommentsItem/MyCommentsItem';
 
 // component that displays a list of the user's list of written comments
 function MyCommentsList() {
@@ -14,7 +16,12 @@ function MyCommentsList() {
 
     console.log(userCommentsList);
     return(
-        <h2>In MyCommentsList</h2>
+        <div>
+            {userCommentsList.map(comment => (
+                <MyCommentsItem key={comment.id} comment={comment} />
+            ))}
+        </div>
+
     )
 }
 
