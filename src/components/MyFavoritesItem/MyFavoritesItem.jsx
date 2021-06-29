52be5fd9e8445eb7155384aca1febaf25fdebdf9
Brandon Wharton <whartonbm@gmail.com>
2021-06-29 13:39:35 -0500

    Made brewery cards clickable to bring user to the BreweryDetails view for the clicked brewery

--- src/components/MyFavoritesItem/MyFavoritesItem.jsx
+++ src/components/MyFavoritesItem/MyFavoritesItem.jsx
@@ -1,3 +1,5 @@
+// hooks
+import { useHistory } from 'react-router';
 // Material-UI components
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
@@ -5,6 +7,7 @@ import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent'
+
 // will likely need card actions for ratings
 
 
@@ -31,10 +34,16 @@ const useStyles = makeStyles((theme) => ({
 function MyFavoritesItem({brewery}) {
     // use the correct Material-UI styles
     const classes = useStyles();
+    // bring in useHistory for navigation
+    const history = useHistory();
 
-    console.log(brewery);
+    // click handler for somebody clicking on a brewery card, sends them to the details page for that brewery
+    const handleClick = (id) => {
+        // send the user to the details page for the specific brewery
+        history.push(`/details/${id}`);
+    }
     return (
-        <Card className={classes.card}>
+        <Card className={classes.card} onClick={() => handleClick(brewery.id) }>
             <CardHeader
                 title={brewery.name}
             >
