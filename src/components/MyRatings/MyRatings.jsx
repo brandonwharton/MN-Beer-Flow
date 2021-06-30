0421fac7364cfdeaf405d7f1dc47bf295963c270
Brandon Wharton <whartonbm@gmail.com>
2021-06-29 21:50:29 -0500

    Used local state to test ratings stars, ratings are being set on click

--- src/components/MyRatings/MyRatings.jsx
+++ src/components/MyRatings/MyRatings.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 // Material-UI components
 import { withStyles } from '@material-ui/core/styles';
 import Rating from '@material-ui/lab/Rating';
 import StarBorderIcon from '@material-ui/icons/StarBorder';
-import FavoriteIcon from '@material-ui/icons/Favorite';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 
@@ -17,6 +16,8 @@ const StyledRating = withStyles({
   },
 })(Rating);
 
+
+// check to see if this is necessary
 function IconContainer(props) {
     const { value, ...other } = props;
     return <span {...other}>{customIcons[value].icon}</span>
@@ -25,21 +26,33 @@ function IconContainer(props) {
 IconContainer.propTypes = {
     value: PropTypes.number.isRequired,
 };
+// through here
+
 
 // component for displaying user brewery ratings, allowing user to adjust ratings fluidly
 function MyRatings({breweryId}) {
 
 
+    const [rating, setRating] = useState('');
+
+    const handleChange = (event) => {
+        setRating(event.target.value);
+    }
+
+    console.log(rating);
     return (
         <div>
             <Box component="fieldset" mb={3} borderColor="transparent">
                 <Typography component="legend">Your Rating</Typography>
                 <Rating
-                    name="customized-empty"
+                    name="simple-controlled"
                     size="large"
-                    defaultValue={0}
-                    precision={0.5}
-                    emptyIcon={<StarBorderIcon fontSize="inherit" />}
+                    value={rating}
+                    // precision={0.5} : play around with this, may be too small
+                    onChange={() => handleChange(event)}
+                    // onChange={(event, newRating) => {
+                    //     setRating(newRating);
+                    // }}
                 />
             </Box>
         </div>
