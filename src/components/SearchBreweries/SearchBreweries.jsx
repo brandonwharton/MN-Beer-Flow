bd59e7edb622af372f5aae0b4d43cf7c1c5cb75a
Brandon Wharton <whartonbm@gmail.com>
2021-07-01 17:47:59 -0500

    Made a new saga for the searched brewery data, made another GGET route, need to do search logic before sending to reducer

--- src/components/SearchBreweries/SearchBreweries.jsx
+++ src/components/SearchBreweries/SearchBreweries.jsx
@@ -5,15 +5,25 @@ import FormControl from '@material-ui/core/FormControl';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 
 
 // component that populates a search bar to search for breweries throughout the entire database and render the results
 function SearchBreweries() {
     const history = useHistory();
+    const dispatch = useDispatch();
+    const breweryList = useSelector(store => store.breweryList);
      // state for holding search string
     const [searchString, setSearchString] = useState('');
     // using params to track searches after a user has made one while still staying on this base component
     const { query } = useParams();
+
+    useEffect(() => {
+        if (query) {
+            dispatch({ type: 'FETCH_SEARCH_RESULTS', payload: query })
+        }
+    }, [query])
 
     // change handler to change the searchString state as user input is filled
     const handleChange = (event) => {
@@ -23,6 +33,7 @@ function SearchBreweries() {
     const handleSubmit = (event) => {
         event.preventDefault();
         history.push(`/search/${searchString}`);
+
     }
 
     console.log('in SearchBreweries: Query: Input', query);
