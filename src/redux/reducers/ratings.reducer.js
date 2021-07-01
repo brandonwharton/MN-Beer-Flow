ff82ecd178bc9c623cfd4cb7f2383487ec59ec66
Brandon Wharton <whartonbm@gmail.com>
2021-07-01 11:27:27 -0500

    Refactored average rating to be done using SQL

--- src/redux/reducers/ratings.reducer.js
+++ src/redux/reducers/ratings.reducer.js
@@ -15,23 +15,14 @@ const userRatingsData = (state = {isFavorite: false, rating: 0}, action) => {
 }
 
 const averageRatingsData = (state = { averageRating: 0}, action) => {
-    console.log('got to averageRatingsData', action.payload);
+
     
     switch(action.type) {
         case 'SET_NO_AVERAGE_RATING':
             return { averageRating: 0 }
         case 'SET_AVERAGE_RATING':
-            const allRatings = action.payload.all_ratings
-            // set rating to 0 if the only rating is a 0
-            if (allRatings[0] === 0) {
-                return { averageRating: 0 };
-            } else {
-                // calculate average rating for whatever length all_ratings array provided
-                const ratingSum = allRatings.reduce( (accumulator, currentValue) => (accumulator + currentValue))
-                const averageRating = ratingSum / allRatings.length
-                // set average rating with calculated value
-                return { averageRating: averageRating }
-            }
+            // average rating comes back from SQL as a string
+            return { averageRating: Number(action.payload.average_rating) }
         default:
             return state;
     }
