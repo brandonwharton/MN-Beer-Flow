4817a44cfe18c85db7e6058a8296472d6cbe3e52
Brandon Wharton <whartonbm@gmail.com>
2021-07-06 16:03:03 -0500

    Brought in AverageRating component into MyFavoritesList to display average ratings along with user ratings

--- src/components/MyFavoritesList/MyFavoritesList.jsx
+++ src/components/MyFavoritesList/MyFavoritesList.jsx
@@ -10,6 +10,7 @@ import Grid from '@material-ui/core/Grid';
 // components
 import MyFavoritesItem from '../MyFavoritesItem/MyFavoritesItem';
 import MyRatings from '../MyRatings/MyRatings';
+import AverageRating from '../AverageRating/AverageRating';
 import NewUserView from '../NewUserView/NewUserView';
 import './MyFavoritesList.css';
 
@@ -118,6 +119,7 @@ function MyFavoritesList() {
                                     <div key={brewery.id}>
                                         <MyFavoritesItem brewery={brewery} />
                                         <MyRatings breweryId={brewery.id} origin={'myFavorites'} rating={brewery.rating} />
+                                        <AverageRating averageRating={brewery.average_rating} />
                                     </div>
                                 ))
                                 :
@@ -125,6 +127,7 @@ function MyFavoritesList() {
                                     <div key={brewery.id}>
                                         <MyFavoritesItem brewery={brewery} />
                                         <MyRatings breweryId={brewery.id} origin={'myFavorites'} rating={brewery.rating} />
+                                        <AverageRating averageRating={brewery.average_rating} />
                                     </div>
                                 ))
                             }
