0af3d8fe05799892367af5c66a785cd9d81b7d17
Brandon Wharton <whartonbm@gmail.com>
2021-07-10 16:03:24 -0500

    adjusted about page

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -10,21 +10,32 @@ function AboutPage() {
 
   return (
     <div className="App-main-position container">
-      <Typography variant="h4" component="h4">
+      <Typography variant="h4" component="h4" gutterBottom>
           Welcome to MN Beer Flow
       </Typography>
       {user.username ?
-      <Typography variant="body1" component="p">
-        This app is designed to allow you to maintain a list of your favorite Minnesota breweries. Any place where you see a small card 
-        with a brewery name and picture, click on it to get more details. You can also comment on the brewery from this details page.
-        You can rate any brewery by clicking on one of the rating stars. Search for new breweries using the link at the top. Go With the Flow
-        lets you randomly select a brewery to help you decide where to visit!
-      </Typography>
+      <>
+        <Typography variant="body1" component="p" gutterBottom>
+          This app is designed to allow you to maintain a list of your favorite Minnesota breweries. Any place where you see a small card 
+          with a brewery name and picture, click on it to get more details. You can also comment on the brewery from this details page.
+          You can rate any brewery by clicking on one of the rating stars. Search for new breweries using the link at the top. Go With the Flow
+          lets you randomly select a brewery to help you decide where to visit!
+        </Typography>
+        <Typography variant="body1" component="p">
+          Use of Location Services is required.
+        </Typography>
+      </>
       :
-      <Typography>
-          This app is designed to allow you to maintain a list of your favorite Minnesota breweries. You can search Minnesota breweries, rate
-          them, comment about them, and choose a random brewery to help you decide where to visit!
-      </Typography>
+      <>
+        <Typography variant="body1" component="p" gutterBottom>
+            This app is designed to allow you to maintain a list of your favorite Minnesota breweries. You can search Minnesota breweries, rate
+            them, comment about them, and choose a random brewery to help you decide where to visit!
+        </Typography>
+        <Typography variant="body1" component="p">
+          Use of Location Services is required.
+        </Typography>
+
+      </>
       }
     </div>
   );
